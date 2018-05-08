fff49cfe895f87679856cfbace4d7858758db7bc
Mirza Leka <mirza_376@hotmail.com>
2018-05-08 23:58:02 +0200

    Starting with regex

---
+++ Resources/js/regex.js
@@ -0,0 +1,14 @@
+
+HEY THERE MY NAME IS JACK SPARROW AND I LIVE IN A LAND FAR,  FAR AWAY. 
+1234556
+\\\\\\\\\\\\
+
+// \d \w \s vs \D \W \S
+
+One color, two colors, three colours. // colou?rs?
+
+This is something  // ^\w+
+Getting the first word
+Out of every line 
+Using Positions 
+Get 4 letter words using boundaries   // \b\w{4}\b
