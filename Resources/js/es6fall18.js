d0b1847a855b559212d947b9d1e72dc6f8c5779d
Mirza Leka <mirza_376@hotmail.com>
2018-09-26 23:58:17 +0200

    Fat arrow update, Rest operator

--- Resources/js/es6fall18.js
+++ Resources/js/es6fall18.js
@@ -25,8 +25,31 @@ var newArr = [...myArr, 4, 5, 6]; // if we'd put myArr without spread operator, 
 // and it would look like this: newArr = [ [1, 2, 3], 4, 5, 6];
 // but since with spread operator we can split array items into idividual item, it will look like this [1,2,3,4,5,6]
 
+// same could be done in ES5 using prototype: 
 
-// Example 3: ES6 object literals
+// newArr = array.prototype.push.apply(newArr,myArr);
+
+
+// Example 3: Rest operator 
+
+	function funky( ...n) {
+		
+		// Using REST operator, we converted all arguments that came after name into an array | put the REST of the params into array
+		console.log(n); 
+		// it's quite different than logging out arguments object because what we have above is number of arguments transformed into array 
+		
+		// outpout:
+		// [1]
+		// [1,2]
+		// [1,2,3]
+			
+	}
+	
+		funky(1);
+		funky(1,2);
+		funky(1,2,3);
+
+// Example 4: ES6 object literals
 
 var name = "Mirza";
 var age = 24;
@@ -55,3 +78,64 @@ var age = 24;
 //	console.log(this.doingRightNow()); // last this was inside obj so it was reffering to that object. This new this is out of all scopes, so it's 
 	// connected with the whole document or should I say to window object. So it will work like user2.doingRightNow();
 
+	
+	let getItems = (action, ...rest) => {
+		
+	action(rest.map( n => rest[n-1] ** rest[n-1] ));
+		
+	}
+	
+	getItems(console.log, 1, 2, 3, 4);
+	
+	// self invoked functions 
+	// https://blog.mgechev.com/2012/08/29/self-invoking-functions-in-javascript-or-immediately-invoked-function-expression/
+	
+	
+	/* set of jquery 
+	array.forEach((val, i) => {
+		$(`.class:eq(${i})`).text(val).click(() => {  
+			$(this).toggleClass("class2");
+		});
+	}); */
+	
+	// Pay attention to these 2 examples
+	
+		var ninja = {
+		name: "Ryu",
+		chop(x) {
+			var _this = this; // requires this line 
+		window.setInterval(function () {
+			if (x > 0) {
+				console.log(_this.name + " chopped the wood"); // now we're using _this.name to get this of an object name and that scope that we lost
+			}
+		}, 500);
+			
+		}
+	};
+	
+	ninja.chop(5);
+	
+	// we can't use this without _this, because it's out of scope, but with arrow function we can do that
+	
+		var ninja = {
+		name: "Ryu",
+		chop(x) {
+		window.setInterval(() => {
+			if (x > 0) {
+				console.log(this.name + " chopped the wood"); // and this will work fine! 
+			}
+		}, 500);
+			
+		}
+	};
+	
+	ninja.chop(5); 
+	
+	
+	
+	
+	
+	
+	
+	
+	
